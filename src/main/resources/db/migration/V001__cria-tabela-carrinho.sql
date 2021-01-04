6d06ca77729cd7cf6752c05d4ad4c1b9bc0d995d
João Pedro <jpmferreira96@gmail.com>
2021-01-04 20:34:53 -0300

    Corrigido script do flyway para postgresql

--- src/main/resources/db/migration/V001__cria-tabela-carrinho.sql
+++ src/main/resources/db/migration/V001__cria-tabela-carrinho.sql
@@ -1,10 +1,8 @@
-create table carrinho (
-	id int SERIAL,
+create table if not exists carrinho (
+	id SERIAL,
 	nome varchar(30) not null,
     fabricante varchar(30)not null,
     pais_fabricacao varchar(30) not null,
     ano int not null,
-    cor varchar(15),
-    
-    primary key(id)
+    cor varchar(15)
 );
