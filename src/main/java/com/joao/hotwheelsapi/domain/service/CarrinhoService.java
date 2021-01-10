23cd8ea11a971527e5859cd3f808e58c4b05a78b
João Pedro <jpmferreira96@gmail.com>
2021-01-10 15:23:49 -0300

    Adicionado exclusão de carrinho

--- src/main/java/com/joao/hotwheelsapi/domain/service/CarrinhoService.java
+++ src/main/java/com/joao/hotwheelsapi/domain/service/CarrinhoService.java
@@ -1,5 +1,26 @@
 package com.joao.hotwheelsapi.domain.service;
 
-public class CarrinhoService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.joao.hotwheelsapi.domain.exception.NegocioException;
+import com.joao.hotwheelsapi.domain.model.Carrinho;
+import com.joao.hotwheelsapi.domain.repository.CarrinhoRepository;
+
+@Service
+public class CarrinhoService {
+	
+	@Autowired CarrinhoRepository carrinhoRepository;
+
+	
+	public Carrinho salvar(Carrinho carrinho) {
+		return carrinhoRepository.save(carrinho);
+	}
+	
+	public void excluir(Long carrinhoId) {
+		if(carrinhoRepository.findById(carrinhoId).isEmpty()) {
+			throw new NegocioException("Carrinho não encontrado!");
+		}
+		carrinhoRepository.deleteById(carrinhoId);
+	}
 }
