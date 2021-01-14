284a983be4a60620c89fb8387aeb8518f68aa3fd
João Pedro <jpmferreira96@gmail.com>
2021-01-13 22:31:53 -0300

    Adicionado alterar e modificado excluir

--- src/main/java/com/joao/hotwheelsapi/api/controller/CarrinhoController.java
+++ src/main/java/com/joao/hotwheelsapi/api/controller/CarrinhoController.java
@@ -5,21 +5,26 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joao.hotwheelsapi.domain.model.Carrinho;
 import com.joao.hotwheelsapi.domain.repository.CarrinhoRepository;
+import com.joao.hotwheelsapi.domain.service.CadastroCarrinhoService;
 
 @RestController
 @RequestMapping("api/carrinho")
 public class CarrinhoController {
 
 	@Autowired CarrinhoRepository carrinhoRepository;
+	
+	@Autowired CadastroCarrinhoService carrinhoService;
 	
 	@GetMapping
 	public List<Carrinho> buscar(){
@@ -44,7 +49,26 @@ public class CarrinhoController {
 		return carrinhoRepository.save(carrinho);
 	}
 	
-	//TODO Adicionar métodos de exclusão de carrinho e alteração
+	@DeleteMapping("/{carrinhoId}")
+	public ResponseEntity<Carrinho> remover (@PathVariable Long carrinhoId) {
+		if(!carrinhoRepository.existsById(carrinhoId)) {
+			return ResponseEntity.notFound().build();
+		}
+		carrinhoService.excluir(carrinhoId);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{carrinhoId}")
+	public ResponseEntity<Carrinho> atualizar(@PathVariable Long carrinhoId, 
+			@RequestBody Carrinho carrinho){
+		if(!carrinhoRepository.existsById(carrinhoId)) {
+			return ResponseEntity.notFound().build();
+		}
+		carrinho.setId(carrinhoId);
+		carrinhoService.salvar(carrinho);
+		return ResponseEntity.ok(carrinho);
+	}
+	
 	
 	
 	
